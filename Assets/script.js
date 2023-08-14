c28a9130ee66afcd913618b6590f63aaf17b857c
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-08-14 11:35:02 -0600

    adding API for forecast and city search

--- Assets/script.js
+++ Assets/script.js
@@ -2,14 +2,78 @@ console.log("JS is loaded");
 // Variables
 const  citySearch = document.getElementById("citySearch")
 const searchBtn = document.getElementById("searchBtn")
+var keyID = 'bd264bda2339694f1d8b05eeddb26285'
+
+let weatherSearch = 'https://api.openweathermap.org/data/2.5/forecast?q='+ citySearch +'&appid='+ keyID
 
 // Functions
-function onClick(e) {
+function clickHandler(e) {
     e.preventDefault()
-    const userInput = citySearch.value 
+    const userInput = citySearch.value.trim()
     console.log (userInput)
+    getGeo(userInput)
+
+}
+//Next fucntion use the user input for the GEO API
+function getGeo(userInput) {
+    var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=${keyID}`
+    console.log(userInput)
+
+    fetch(apiURL)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+    cityForecast(data[0])
+
+    })
     
 }
+//Use the geoAPI data for the weatherAPI
+function cityForecast(cityCoords) {
+    //var cityCoords= data[0]
+    console.log(cityCoords)
+var {lat ,lon } = cityCoords
+var city = cityCoords.name
+    //fetch
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${keyID}`
+    
+
+    fetch(apiURL)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        currentDay(data.list[0].main, city)
+        forecast(data.list)
+  
+
+    })
+}
+//currentDay is for 1 day; name, date, imgIcon, temp, humidity, wind into a card
+    function currentDay(weather, city) {
+        console.log(weather, city)
+        //create variables that rep the data
+        var temp= weather.temp
+        var humidity= weather.humidity
+    console.log(temp, humidity)
+
+    //create the card (line 26 in html)
+var card = document.createElement('div')
+        card.classList.add('card')
+        
+        
+
+// display the data to the card, then need to append
+    }
+
+//forecast is for 5 days; date, imgIcon, temp, humidity, wind into 5 cards
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
 // WHEN I search for a city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
 
@@ -22,9 +86,21 @@ function onClick(e) {
 // Special Functions
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+// if (localStorage.getItem("cities")) {
+// 	storedCities = JSON.parse(localStorage.getItem("cities"));
+// 	console.log(storedCities);
+// 	for (var i = 0; i < storedCities.length; i++) {
+// 		lastCitySearched = storedCities.length - 1;
+// 		var lastCity = storedCities[lastCitySearched];
+// 	}
+// } else {
+// 	cities;
+// }
+// renderLastCityInfo();
+// console.log("cities", cities);
 
-// Busniess logic
-searchBtn.addEventListener("click", onClick);
+// // Busniess logic
+searchBtn.addEventListener("click", clickHandler);
 
 
 
